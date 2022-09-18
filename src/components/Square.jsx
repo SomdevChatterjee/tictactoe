8dfb0d1ccfa27d4911757153785fa51ed808639f
Sam <somdevchatterjee101@gmail.com>
2022-09-18 17:51:42 +0530

    styled the app and added a board component

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Square.style.css";
+const Square = ({value}) => {
+    console.log();
+  return (
+        <button type='button'>{value}</button>
+  )
+}
+
+export default Square
